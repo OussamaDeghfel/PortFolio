0ee15a1465720f64ea35cfdb05f009a4bcdc4ba8
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-09-30 17:28:10 +0100

    add the logic of sliding change betwen project

--- src/components/Projects/MyProjects.jsx
+++ src/components/Projects/MyProjects.jsx
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import { BiArrowFromLeft, BiArrowFromRight } from "react-icons/bi";
-import ComingSoon from '../../assets/projects/ComingSoon.jpg'
-
+import ComingSoon from "../../assets/projects/ComingSoon.jpg";
+import MovieSite from "../../assets/projects/MovieSite.jpg";
+import JobFinder from "../../assets/projects/JobFinder.jpg";
+import FooDy from "../../assets/projects/FooDy.png";
+import IPAddressTracker from "../../assets/projects/IPAddressTracker.jpg";
 
 const MyProjects = () => {
+  const [counter, setCounter] = useState(0);
+  const [projectShow, setProjectShow] = useState("");
+
+
+  const images = [MovieSite, JobFinder, FooDy, IPAddressTracker, ComingSoon];
+
+  useMemo(() => {
+    setProjectShow(images[counter]);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [counter]);
+
+  console.log("Counter",counter)
+  console.log("projectShow",projectShow)
+
   return (
     <div
       name="projects"
@@ -17,22 +34,38 @@ const MyProjects = () => {
           These are the projects I have built using the skills outlined above
         </p>
         <div className="w-full h-full flex p-8 px-20 justify-between items-center">
-          <BiArrowFromRight size={40} className="cursor-pointer hover:rounded-full hover:border-2 hover:border-white duration-50 hover:shadow-sm hover:shadow-blue-300"  />
+          <BiArrowFromRight
+            size={40}
+            onClick={() => setCounter(counter - 1)}
+            className="cursor-pointer hover:rounded-full hover:border-2 hover:border-white duration-50 hover:shadow-sm hover:shadow-blue-300"
+          />
           <div className="flex flex-col justify-center items-center">
             <div className="h-96 w-[70vh] border border-slate-600 rounded-md shadow-md shadow-blue-300">
-            <div className='flex'>
-              <img src={ComingSoon} alt="comming soon project" className='rounded-md h-64 w-full' />
-            </div>
-            <div className="flex justify-between items-center p-4">
+              <div className="flex">
+                <img
+                  src={projectShow}
+                  alt="comming soon project"
+                  className="rounded-md h-64 w-full"
+                />
+              </div>
+              <div className="flex justify-between items-center p-4">
                 <p> Project title</p>
                 <div className="flex flex-col space-y-2">
-                    <button className="bg-slate-800 border-2 border-white rounded-md px-4 py-2">Visit</button>
-                    <button className="bg-slate-800 border-2 border-white rounded-md px-4 py-2">Code</button>
-                </div> 
-            </div>
+                  <button className="bg-slate-800 border-2 border-white rounded-md px-4 py-2">
+                    Visit
+                  </button>
+                  <button className="bg-slate-800 border-2 border-white rounded-md px-4 py-2">
+                    Code
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
-          <BiArrowFromLeft size={40} className="cursor-pointer hover:rounded-full hover:border-2 hover:border-white duration-50 hover:shadow-sm hover:shadow-blue-300" />
+          <BiArrowFromLeft
+            size={40}
+            onClick={() => setCounter(counter + 1)}
+            className="cursor-pointer hover:rounded-full hover:border-2 hover:border-white duration-50 hover:shadow-sm hover:shadow-blue-300"
+          />
         </div>
       </div>
     </div>
