91abe2461dfad71bbcc043fe3971c44eac3a0889
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-09-30 16:49:14 +0100

    enhancement

--- src/components/Projects/MyProjects.jsx
+++ src/components/Projects/MyProjects.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { BiArrowFromLeft, BiArrowFromRight } from "react-icons/bi";
+import ComingSoon from '../../assets/projects/ComingSoon.jpg'
+
 
 const MyProjects = () => {
   return (
@@ -14,17 +16,23 @@ const MyProjects = () => {
         <p className="py-4">
           These are the projects I have built using the skills outlined above
         </p>
-        <div className="w-full h-full flex border-2 border-red-700 p-8 px-20 justify-between items-center">
-          <BiArrowFromRight size={40} className="cursor-pointer hover:rounded-full hover:border-2 hover:border-white duration-50"  />
-          <div className="grid grid-cols-2 gap-8">
-            <div className="h-96 w-72 border border-blue-500 ">
-              image for the project
+        <div className="w-full h-full flex p-8 px-20 justify-between items-center">
+          <BiArrowFromRight size={40} className="cursor-pointer hover:rounded-full hover:border-2 hover:border-white duration-50 hover:shadow-sm hover:shadow-blue-300"  />
+          <div className="flex flex-col justify-center items-center">
+            <div className="h-96 w-[70vh] border border-slate-600 rounded-md shadow-md shadow-blue-300">
+            <div className='flex'>
+              <img src={ComingSoon} alt="comming soon project" className='rounded-md h-64 w-full' />
             </div>
-            <div className="h-96 w-72 border border-blue-500 ">
-              description about the project
+            <div className="flex justify-between items-center p-4">
+                <p> Project title</p>
+                <div className="flex flex-col space-y-2">
+                    <button className="bg-slate-800 border-2 border-white rounded-md px-4 py-2">Visit</button>
+                    <button className="bg-slate-800 border-2 border-white rounded-md px-4 py-2">Code</button>
+                </div> 
+            </div>
             </div>
           </div>
-          <BiArrowFromLeft size={40} className="cursor-pointer hover:rounded-full hover:border-2 hover:border-white duration-50" />
+          <BiArrowFromLeft size={40} className="cursor-pointer hover:rounded-full hover:border-2 hover:border-white duration-50 hover:shadow-sm hover:shadow-blue-300" />
         </div>
       </div>
     </div>
