cd165c94f9a2908e098cf83538857f84cd6a4cab
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-09-30 14:24:39 +0100

    cahnge some details

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,12 +6,12 @@ const Home = () => {
     <div name="home" className="w-full h-screen bg-[#0a192f]">
       {/* Container */}
       <div className="max-w-[700px] mx-auto px-5 flex flex-col justify-center text-center h-full ">
-        <p className="text-blue-400 text-xl space-y-10"> Say My Name</p>
+        <p className="text-blue-400 text-xl space-y-10"> Full Name</p>
         <h1 className="text-6xl font-bold text-[#ccd6f6] uppercase m-3">
           Deghfel Oussama
         </h1>
         <h2 className="text-4xl font-bold text-[#8892b0]">
-          I'm a FrontEnd Developer
+          FrontEnd Web Developer
         </h2>
         <p className="text-[#8892b0] py-4 max-w-[700px]">
           I'm a passionate front-end developer with a knack for turning design
