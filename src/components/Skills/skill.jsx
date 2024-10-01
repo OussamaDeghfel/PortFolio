088087c5515dddd0c2bb54c4f7b0482e4a6e071b
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-10-01 16:53:33 +0100

    enh: skills styling

--- src/components/Skills/skill.jsx
+++ src/components/Skills/skill.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Skill = ({ icon, name }) => {
     return (
-      <div className="w-full h-full p-4 shadow-sm shadow-[#040c16] hover:shadow-md hover:shadow-[#040c16] hover:scale-105 duration-500 rounded-md">
-        <img className="w-20 mx-auto" src={icon} alt={name} />
+      <div className="w-32 h-36 p-4 shadow-sm shadow-[#040c16] hover:shadow-md hover:shadow-[#040c16] hover:scale-105 duration-500 rounded-md">
+        <img className="w-14 mx-auto" src={icon} alt={name} />
         <p className="my-4">{name}</p>
       </div>
     );
