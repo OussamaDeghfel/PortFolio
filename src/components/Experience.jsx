cd62a33b96498ef0024d3bf99d8344d27ae9280e
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-11-03 21:18:16 +0100

    put the experience inside array and map throught to reduce the multiple element

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,8 +1,50 @@
 import React from "react";
 
 const Experience = () => {
+  const ExperienceData = [
+    {
+      id: 1,
+      title: "Junior Frontend Web Developer",
+      company: "3M Express",
+      period: "08/2024 - 10/2024",
+      description: `As a Junior FrontEnd Web Developer at 3M Express, I am involved in developing and maintaining user-friendly web interfaces. I apply my knowledge to create engaging and interactive websites, ensuring
+              a seamless user experience. Collaborate with the development team
+              to produce high-quality code and user interfaces. Participate in
+              the design and implementation of new website features and
+              functionalities. Utilize version control systems such as Git for
+              efficient code management and collaboration. Stay updated with the
+              latest trends and technologies in front-end development to enhance
+              web applications.`,
+    },
+    {
+      id: 2,
+      title: "Internship in Frontend Role",
+      company: "3M Express",
+      period: "03/2024 - 07/2024",
+      description: `During my internship at 3M Express, I gained valuable hands-on
+              experience in front-end web development. I collaborated with
+              senior developers to work on various projects, where I was
+              responsible for implementing and testing user interfaces using
+              HTML, CSS, and JavaScript`,
+    },
+    {
+      id: 3,
+      title: "Graphic Designer",
+      company: "Freelancer",
+      period: "2019 - 2022",
+      description: `I have a strong background in graphic design, where I developed
+              expertise in creating visually engaging layouts and mastering
+              color theory. This experience taught me how to balance aesthetics
+              with user needs. Now, I’m transitioning into frontend development,
+              eager to build dynamic, interactive web experiences.`,
+    },
+  ];
+
   return (
-    <div name="experience" className="w-full h-screen bg-[#0a192f] text-gray-300">
+    <div
+      name="experience"
+      className="w-full h-screen bg-[#0a192f] text-gray-300"
+    >
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
         <div>
           <div className="pb-8">
@@ -12,66 +54,25 @@ const Experience = () => {
           </div>
           <div></div>
         </div>
+
         <div className="max-w-[1000px] grid grid-cols-1 w-full gap-8">
-          <div className="grid grid-cols-1 space-y-4 border-2 border-slate-500 hover:shadow-white hover:text-white hover:border-cyan-50 duration-300 hover:bg-slate-800 p-8 rounded-md shadow-md">
-            <div className="flex flex-col">
-              <h1 className="text-4xl font-bold">
-                Junior Frontend Web Developer
-              </h1>
-              <h1 className="text-2xl italic font-medium text-gray-400">
-                3M Express
-              </h1>
-              <span className="italic text-gray-400">08/2024 - 10/2024,</span>
+          {ExperienceData.map((experience) => (
+            <div
+              key={experience.id}
+              className="grid grid-cols-1 space-y-4 border-2 border-slate-500 hover:shadow-white hover:text-white hover:border-cyan-50 duration-300 hover:bg-slate-800 p-8 rounded-md shadow-md"
+            >
+              <div className="flex flex-col">
+                <h1 className="text-4xl font-bold">{experience.title}</h1>
+                <h1 className="text-2xl italic font-medium text-gray-400">
+                  {experience.company}
+                </h1>
+                <span className="italic text-gray-400">
+                  {experience.period}
+                </span>
+              </div>
+              <p>{experience.description}</p>
             </div>
-            <p>
-              As a Junior FrontEnd Web Developer at 3M Express, I am involved in
-              developing and maintaining user-friendly web interfaces. I apply
-              my knowledge to create engaging and interactive websites, ensuring
-              a seamless user experience. Collaborate with the development team
-              to produce high-quality code and user interfaces.Participate in
-              the design and implementation of new website features and
-              functionalities.Utilize version control systems such as Git for
-              efficient code management and collaboration.Stay updated with the
-              latest trends and technologies in front-end development to enhance
-              web applications.
-            </p>
-          </div>
-
-          <div className="grid grid-cols-1 space-y-4 border-2 border-slate-500 hover:shadow-white hover:text-white hover:border-cyan-50 duration-300 hover:bg-slate-800 p-8 rounded-md shadow-md">
-            <div className="flex flex-col">
-              <h1 className="text-4xl font-bold">
-                Internship in Frontend Role
-              </h1>
-              <h1 className="text-2xl italic font-medium text-gray-400">
-                3M Express
-              </h1>
-              <span className="italic text-gray-400">03/2024 - 07/2024</span>
-            </div>
-            <p>
-              During my internship at 3M Express, I gained valuable hands-on
-              experience in front-end web development. I collaborated with
-              senior developers to work on various projects, where I was
-              responsible for implementing and testing user interfaces using
-              HTML, CSS, and JavaScript
-            </p>
-          </div>
-
-          <div className="grid grid-cols-1 space-y-4 border-2 border-slate-500 hover:shadow-white hover:text-white hover:border-cyan-50 duration-300 hover:bg-slate-800 p-8 rounded-md shadow-md">
-            <div className="flex flex-col">
-              <h1 className="text-4xl font-bold">Graphic Designer</h1>
-              <h1 className="text-2xl italic font-medium text-gray-400">
-                Freelancer
-              </h1>
-              <span className="italic text-gray-400">2019 - 2022</span>
-            </div>
-            <p>
-              I have a strong background in graphic design, where I developed
-              expertise in creating visually engaging layouts and mastering
-              color theory. This experience taught me how to balance aesthetics
-              with user needs. Now, I’m transitioning into frontend development,
-              eager to build dynamic, interactive web experiences.
-            </p>
-          </div>
+          ))}
         </div>
       </div>
     </div>
